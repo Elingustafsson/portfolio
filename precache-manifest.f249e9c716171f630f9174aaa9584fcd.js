b3e4df3e1d362944faa3dd7be9965a09f5078eaf
Elin Gustafsson <elgusen@hotmail.com>
2018-11-09 17:46:35 +0100

    Updates

---
+++ precache-manifest.f249e9c716171f630f9174aaa9584fcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f579f1b2503081d7fa0a2beb756cf32",
+    "url": "/portfolio/static/media/paperplane.4f579f1b.png"
+  },
+  {
+    "revision": "711ec4941fda973a6ee83b964b90ffe6",
+    "url": "/portfolio/static/media/Elin.711ec494.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "dd76e260206bfebe0c13",
+    "url": "/portfolio/static/js/main.dd76e260.chunk.js"
+  },
+  {
+    "revision": "4ddfddf36b8fcf811772",
+    "url": "/portfolio/static/js/1.4ddfddf3.chunk.js"
+  },
+  {
+    "revision": "dd76e260206bfebe0c13",
+    "url": "/portfolio/static/css/main.a959dbc4.chunk.css"
+  },
+  {
+    "revision": "a2f10343b4ae0f908b9d8e6c53bf0b98",
+    "url": "/portfolio/index.html"
+  }
+];
